948150f254dd931ae9d0e335ffa88d6d0649a46d
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-14 18:55:05 -0500

    Updated Code to Start Writing to File

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,48 @@
-// array of questions for user
+// require fs 
+var fs = require("fs");
+
+// // instsall inquirer package
+var inquirer = require("inquirer");
+var newTitle= "hi";
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your README Title?",
+      name: "title"
+    },
+  ])
+  .then(function(response) {
+    console.log(response);
+    console.log(response.title);
+    newTitle = response.title;
+    init();
+  });
+
+// // array of questions for user
 const questions = [
 
 ];
 
-// function to write README file
+// // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        // console.log("Success!");
+      
+      });
 }
 
 // function to initialize program
 function init() {
-
+    console.log("thisisaninit");
+    console.log(newTitle);
+    writeToFile("readme.md", newTitle);
 }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+
