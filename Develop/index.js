108dccb4e8781c6a2369155aa82b43df35f84525
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-15 20:02:37 -0500

    created array of licenses and badges

--- Develop/index.js
+++ Develop/index.js
@@ -3,92 +3,56 @@ const fs = require("fs");
 
 // // instsall inquirer package
 const inquirer = require("inquirer");
-const markdown = require ("../Develop/utils/generateMarkdown");
 
+// bring in markdown  module
+const markdown = require("../Develop/utils/generateMarkdown");
 
-
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "What is your README Title?",
-//       name: "title"
-//     },
-//       {
-//         type: "input",
-//         message: "Installation:",
-//         name: "installation"
-//       },
-//       {
-//         type: "input",
-//         message: "Usage:",
-//         name: "usage"
-//       },
-//     //   make this a list
-//       {
-//         type: "input",
-//         message: "License:",
-//         name: "license"
-//       },
-//       {
-//         type: "input",
-//         message: "Contributing:",
-//         name: "contributing"
-//       },
-//       {
-//         type: "input",
-//         message: "Tests:",
-//         name: "tests"
-//       },
-//       {
-//         type: "input",
-//         message: "Questions:",
-//         name: "questions"
-//       },
-//   ])
-//   .then(function(response) {
-//     console.log(response);
-//     console.log(response.title);
-//     console.log(response.description);
-//     console.log(response.toc);
-//     console.log(response.installation);
-//     console.log(response.usage);
-//     console.log(response.license);
-//     console.log(response.contributing);
-//     console.log(response.tests);
-//     console.log(response.questions);
-//     newTitle = response.title;
-//     description=response.description;
-//     init();
-//   });
-
+// append these responses to questions section
+var userInfo = [
+    {
+        type: "input",
+        message: "What is your GitHub Username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Additional contact info/ways to be reached:",
+        name: "additionalinfo"
+      },
+]
 // // array of questions for user
-const questions = [
+var questions = [
     {
       type: "input",
-      message: "What is your README Title?",
+      message: "What is your README title?",
       name: "title"
     },
       {
         type: "input",
-        message: "Installation:",
+        message: "Enter installation information:",
         name: "installation"
       },
       {
         type: "input",
-        message: "Usage:",
+        message: "Enter usage information:",
         name: "usage"
       },
-    //   make this a list
+    //   make this a list // import a badge?
       {
-        type: "input",
-        message: "License:",
-        name: "license"
+        type: "list",
+        name: "license",
+        message: "Select license type:",
+        choices: ["copyleft","lpgl","MIT","permissive","proprietary","public"]
       },
       {
         type: "input",
-        message: "Contributing:",
-        name: "contributing"
+        message: "Enter contributors:",
+        name: "contributors"
       },
       {
         type: "input",
@@ -120,17 +84,17 @@ function init() {
     inquirer
         .prompt(questions)
         .then(function(response) {
-            console.log(response);
-            console.log(response.title);
-            console.log(response.description);
-            console.log(response.toc);
-            console.log(response.installation);
-            console.log(response.usage);
-            console.log(response.license);
-            console.log(response.contributing);
-            console.log(response.tests);
-            console.log(response.questions);
-            console.log("thisisaninit");
+            // console.log(response);
+            // console.log(response.title);
+            // console.log(response.description);
+            // console.log(response.toc);
+            // console.log(response.installation);
+            // console.log(response.usage);
+            // console.log(response.license);
+            // console.log(response.contributing);
+            // console.log(response.tests);
+            // console.log(response.questions);
+            // console.log("thisisaninit");
             writeToFile("readme.md", markdown(response));
 
           });
